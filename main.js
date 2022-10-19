822cf87d7726d5c81dcf18741f6dc8d0196eb487
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-10-19 16:07:57 +0200

    testing first way

---
+++ main.js
@@ -0,0 +1,19 @@
+const priceKmEuro = 0.21
+const u18DiscountPerc = 0.2
+const o65DiscountPerc = 0.4
+
+let price = distance * priceKmEuro
+
+let discountedPriceU18 = price * u18DiscountPerc
+
+let discountedPriceO65 = price * o65DiscountPerc
+
+
+let distance = 10
+let age = 19
+
+console.log()
+
+function outputPrice(){
+
+}
