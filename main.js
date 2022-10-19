66b130a1e5df26ab4b1eaa3c061042c7ce011e4e
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-10-19 18:08:49 +0200

    testing the switch method

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/********************************
+FIRST METHOD - MANUAL INPUT - ELSE IF
+********************************/
 
 // let age = 66
 // let distance = 20
@@ -26,6 +29,43 @@
 
 // }
 
+/********************************
+SECOND METHOD - FORM INPUT - ELSE IF
+********************************/
+
+// const priceKmEuro = 0.21
+// const u18DiscountPerc = 0.2
+// const o65DiscountPerc = 0.4
+
+// function calculateTotalPrice(){
+//     // Selecting the input elements and get the values
+//     var distanceValue = document.getElementById("distance").value;
+//     console.log("check distance", distanceValue)
+
+//     var ageValue = document.getElementById("age").value;
+//     console.log("check age", ageValue)
+    
+//     let price = distanceValue * priceKmEuro
+
+
+//     if(ageValue < 18){
+//         totalPrice = price - (price * u18DiscountPerc)
+
+//     } else if(ageValue > 65){
+//         totalPrice = price - (price * o65DiscountPerc)
+
+//     } else{
+//         totalPrice = price
+
+//     }
+
+//     console.log(totalPrice)
+// }
+
+/********************************
+THIRD METHOD - SWITCH / CASE 
+********************************/
+
 const priceKmEuro = 0.21
 const u18DiscountPerc = 0.2
 const o65DiscountPerc = 0.4
@@ -40,43 +80,24 @@ function calculateTotalPrice(){
     
     let price = distanceValue * priceKmEuro
 
-
     if(ageValue < 18){
+        let ageUnderOver = "under18"
+
+    } else(ageValue > 65){
+        ageUnderOver = "over65"
+
+    }
+
+    console.log("check ageUnderOver variable", ageUnderOver)
+
+
+    switch(ageUnderOver){
+        case under18:
         totalPrice = price - (price * u18DiscountPerc)
 
-    } else if(ageValue > 65){
-        totalPrice = price - (price * o65DiscountPerc)
-
-    } else{
-        totalPrice = price
-
+        case over65:
+        totalPrice = price - (price * u18DiscountPerc)
     }
 
     console.log(totalPrice)
 }
-
-
-
-
-// switch (age) {
-//     case age < 18:
-        
-//         totalPrice = price - (price * u18DiscountPerc)
-//         break
-
-//     case (age > 65):
-//         totalPrice = price - (price * o65DiscountPerc)
-        
-//         break
-
-//     default:
-//         totalPrice = price
-// }
-
-
-
-// console.log(totalPrice)
-
-// function outputPrice(){
-
-// }
