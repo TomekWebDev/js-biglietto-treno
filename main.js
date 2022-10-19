19baee97dd98b2bca7ed885d21ae6a178a09fd69
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-10-19 16:35:05 +0200

    else if way done

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
+let distance = 100
+let age = 66
+
 const priceKmEuro = 0.21
 const u18DiscountPerc = 0.2
 const o65DiscountPerc = 0.4
 
 let price = distance * priceKmEuro
 
-let discountedPriceU18 = price * u18DiscountPerc
+let discountedPriceU18 = price - (price * u18DiscountPerc)
 
-let discountedPriceO65 = price * o65DiscountPerc
+let discountedPriceO65 = price - (price * o65DiscountPerc)
+
+console.log(age)
+
+if(age < 18){
+    totalPrice = price - (price * u18DiscountPerc)
+
+} else if(age > 65){
+    totalPrice = price - (price * o65DiscountPerc)
+
+} else{
+    totalPrice = price
+
+}
 
 
-let distance = 10
-let age = 19
+// switch (age) {
+//     case age < 18:
+        
+//         totalPrice = price - (price * u18DiscountPerc)
+//         break
 
-console.log()
+//     case (age > 65):
+//         totalPrice = price - (price * o65DiscountPerc)
+        
+//         break
+
+//     default:
+//         totalPrice = price
+// }
+
+
+
+console.log(totalPrice)
 
 function outputPrice(){
 
